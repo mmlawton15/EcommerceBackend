a48ad60a1b1160d18b0cafb8345cd508a2793e47
Mary Margaret <mmlawton15@gmail.com>
2022-02-02 10:44:11 -0500

    error denied access. working on my category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,35 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  console.log('======================')
+  Category.findAll({
+    attributes: [
+      'id',
+      'category_name',
+      [sequelize.literal('(SELECT * FROM Category')]
+    ]
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  console.log('======================')
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  console.log('======================')
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  console.log('======================')
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  console.log('======================')
 });
 
 module.exports = router;
