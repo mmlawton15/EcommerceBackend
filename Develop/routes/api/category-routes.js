4b713c687462bd9ed82f9f3104bb619b6f34b2b9
Mary Margaret <mmlawton15@gmail.com>
2022-02-04 10:30:47 -0500

    added routes to index.js, added requirements to server.js, finished category route connections

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
     attributes: [
       'id',
       'category_name',
-      //[sequelize.literal('(SELECT * FROM Category')] //unsure how to reference products here JOIN
+      //[sequelize.literal('(SELECT * FROM Category')] //need to use a join to reference products but after refactoring the sequelize.literal is breaking my code
     ]
   }).then((taco) => {
     console.log(taco);
@@ -34,41 +34,84 @@ router.get('/:id', (req, res) => {
       'category_name'
       //[sequelize.literal('(SELECT COUNT(*) FROM Product WHERE Category.id = Product.category_id)')] //i know this is wrong
     ],
-    // include: [
-    //   {
-    //     model: Category,
-    //     attributes: ['id', 'category_name'], //PROBABLY DELETE THIS AFTER UPDATING INDEX.JS
-    //     include: {
-    //       model: Product,
-    //       attributes: ['product_name']
-    //     }
-    //   },
-    //   {
-    //     model: Product,
-    //     attributes: ['product_name'] //trying to list out the products tied to this category?
-    //   }
-    // ]
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'], //PROBABLY DELETE THIS AFTER UPDATING INDEX.JS
+      }
+    ]
   })
   .then(dbCategoryData => {
-    console.table(dbCategoryData);
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No Category found with this id' });
+      return;
+    }
     res.json(dbCategoryData);
   })
-  //res.json('mary broke it')
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
   console.log('======================')
+  Category.create({
+    category_name: req.body.category_name //is this creating a new category in the Category table?
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
   console.log('======================')
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'No category found with this id'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .cetch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
   console.log('======================')
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No category found with that id!'})
+      return;
+    }
+    res.json(dbCategoryData)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const categoryRoutes = require('./api/category-routes.js')
+const productRoutes = require('./api/product-routes.js')
+const tagRoutes = require('./api/tag-routes.js')
 
 router.use('/api', apiRoutes);
+router.use('/category', categoryRoutes); //trying to mimic the lesson content here but not sure what these are referencing
+router.use('/product', productRoutes);
+router.use('/tag', tagRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
