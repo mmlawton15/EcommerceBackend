97bd7214ce36b73d8f1d59fe1a50a2335a24f05e
Mary Margaret <mmlawton15@gmail.com>
2022-02-06 10:33:27 -0500

    wrapped app.listen in sequelize

--- Develop/server.js
+++ Develop/server.js
@@ -24,7 +24,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-//wrap this below function in sequelize
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
