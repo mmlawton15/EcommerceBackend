83a95889c4dd574b63a32716a08d7b4e2a853e5c
Mary Margaret <mmlawton15@gmail.com>
2022-04-21 09:30:39 -0400

    adding stack and details to readme file

--- README.md
+++ README.md
@@ -4,7 +4,13 @@
 This project is our 13th challenge of the bootcamp where we had to work off starter code to create the backend for an eCommerce website. We were specifically focused on using what we learned during our Object-Relational Mapping chapter.
 
 ## What's the point and what did I learn?
+This project is for a business t be able to keep track of their inventory and give the pre-existing front end website functionality. I learned about ORM and how it communicates betweent e backend and frontend.
 
-## What does it look like?
+## Stack
+Backend:
+- Sequelize
+- Javascript
+- Node.js
 
 ## Where can I find this?
+This can be found at my github: https://github.com/mmlawton15/EcommerceBackend
--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-// DONE - GIVEN a functional Express.js API
-// DONE - WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
-// DONE - THEN I am able to connect to a database using Sequelize
-// DONE - WHEN I enter schema and seed commands
-// DONE - THEN a development database is created and is seeded with test data
-// DONE - WHEN I enter the command to invoke the application
-// DONE - THEN my server is started and the Sequelize models are synced to the MySQL database
-// WHEN I open API GET routes in Insomnia for categories, products, or tags
-// THEN the data for each of these routes is displayed in a formatted JSON
-// WHEN I test API POST, PUT, and DELETE routes in Insomnia
-// THEN I am able to successfully create, update, and delete data in my database
-
 const express = require('express');
 const routes = require('./Develop/routes');
 const sequelize = require('./Develop/config/connection');
